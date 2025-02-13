f08b768a63b12a8a12c72d44cc8ad8d12a47c346
Fabian Montaño <fabiantccfa@gmail.com>
2025-02-12 21:40:06 -0600

    add: implement task listing functionality with tasks status filters

--- index.js
+++ index.js
@@ -113,6 +113,82 @@ function remove(body) {
     console.log(`Task removed successfully (ID: ${idTask})`);
 }
 
+function listHandler(body) {
+    if (body.length === 0) {
+        listAll();
+        return;
+    }
+
+    switch (body[0]) {
+        case 'todo':
+            listTodo();
+            break;
+    
+        case 'in-progress':
+            listInProgress();
+            break;
+        
+        case 'done':
+            listDone();
+            break;
+
+        default:
+            throw new Error('[list] Command not found');
+    }
+}
+
+function listAll() {
+    const tasks = JSON.parse(readTaskJSON());
+    
+    if (tasks.length === 0) {
+        console.log('There are no tasks, add one!');
+        return;
+    }
+
+    tasks.forEach(task => {
+        console.log(`ID: ${task.id} | Description: ${task.description} | Status: ${task.status}`);
+    });
+}
+
+function listTodo() {
+    const tasks = JSON.parse(readTaskJSON());
+
+    const todoTasks = tasks.filter(task => task.status === 'todo');
+    if (todoTasks.length === 0) {
+        console.log('You have no tasks TODO');
+    }
+
+    todoTasks.forEach(task => {
+        console.log(`ID: ${task.id} | Description: ${task.description} | Status: ${task.status}`);
+    });
+}
+
+function listInProgress() {
+    const tasks = JSON.parse(readTaskJSON());
+
+    const inProgressTasks = tasks.filter(task => task.status === 'in-progress');
+    if (inProgressTasks.length === 0) {
+        console.log('You have no tasks IN PROGRESS');
+    }
+
+    inProgressTasks.forEach(task => {
+        console.log(`ID: ${task.id} | Description: ${task.description} | Status: ${task.status}`);
+    });
+}
+
+function listDone() {
+    const tasks = JSON.parse(readTaskJSON());
+
+    const doneTasks = tasks.filter(task => task.status === 'done');
+    if (doneTasks.length === 0) {
+        console.log('You have no tasks DONE');
+    }
+
+    doneTasks.forEach(task => {
+        console.log(`ID: ${task.id} | Description: ${task.description} | Status: ${task.status}`);
+    });
+}
+
 function readUserInput(arg) {
     try {
         const command = arg[2];
@@ -122,12 +198,19 @@ function readUserInput(arg) {
             case 'add':
                 add(body);
                 break;
+
             case 'update':
                 update(body);
                 break;
+
             case 'delete':
                 remove(body);
                 break;
+
+            case 'list':
+                listHandler(body);
+                break;
+
             default:
                 throw new Error('Command not found');
                 break;
